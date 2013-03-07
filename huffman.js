93e936a7668f68b14aeb229565553625333fd1d2
rasheedh <rashapythonhunter@gmail.com>
2013-03-07 15:15:51 +0530

    first commit

---
+++ huffman.js
@@ -0,0 +1,96 @@
+function tree(left,right,data){
+        this.left=left
+        this.right=right
+        this.data=data
+        }
+function encode(z,i){
+        dig = []
+        
+        while (i!=z.data[0]){ 
+             if (z.left.data[0].indexOf(i)!=-1){
+                  dig.push('0')
+                  z=z.left}
+             else if (z.right.data[0].indexOf(i)!=-1){
+                  dig.push('1')
+                  z=z.right}  
+             }
+        hai = dig.join('')
+        console.log("haaaai=",hai)        
+        return hai
+        }
+
+function decode(r,z){
+     nw = z
+     word = ''
+     for (var i in r){
+         if (r[i] == "1"){
+              z = z.right}
+         else if (r[i] == "0"){
+              z = z.left}
+         if (z.left == null || z.right == null){
+              word = word + z.data[0] 
+              z = nw}
+        }
+     return word
+        }
+function sorting(o){
+        l = []
+        
+        for (var i in o){
+                l.push(new tree(null,null,o[i]))
+                }
+                
+                //l=reverse()
+                console.log(l)
+                
+        while (l.length>1){
+                f = l.pop()
+                s = l.pop()
+                
+                //console.log(f["data"][1])}
+                h = [f.data[0]+s.data[0],f.data[1]+s.data[1]]
+                //console.log(h)
+                l.push(new tree(f,s,h))
+                l.sort(function(c,d){return d.data[1]-c.data[1]})
+                }
+        //console.log(l)
+        z = l.pop()
+        //console.log(z)
+        return z
+
+        }
+
+function main(){
+    d = {}
+    o = []
+    res = ''
+    q = ''
+    str = "rasheedh"
+    
+    for (var c in str){
+        if (str[c] in d){
+            d[str[c]] =d[str[c]]+1}
+        else  
+            d[str[c]]= 0}
+    for (var key in d){
+        o.push([key,d[key]])          
+        }
+        
+    o.sort(function(a,b){return b[1]-a[1]})
+        
+    z=sorting(o)
+    for(var c in str){
+        d = encode(z,str[c])
+        toString(d)
+        res=res+d 
+        //console.log(str[c])
+        }   
+    console.log("Here is the encoded string",res)
+        r=res
+        q = decode(r,z)
+    console.log ("Here is the decoded digit",q)
+    //console.log("value of z is")
+    //console.log(z)
+    //console.log(o)
+        }
+main()
